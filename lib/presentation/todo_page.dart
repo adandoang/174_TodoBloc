c89f1a9df0994afa89f9977e1f4356f732b76a33
adandoang <akhdanjzif@gmail.com>
2025-05-08 19:31:46 +0700

    menghapus import package yang tidak diperlukan dan sedikit merapihkan code di bagian kurung tutup

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/bloc/todo_bloc.dart';
 
@@ -153,7 +152,9 @@ class Todopage extends StatelessWidget {
                 ],
               )
             ],
-          ),)),
+          ),
+        )
+      ),
     );
   }
 }
