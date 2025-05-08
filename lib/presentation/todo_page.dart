17f2438696f91ed4050f10a7ee99b32b92906977
adandoang <akhdanjzif@gmail.com>
2025-05-08 18:49:55 +0700

    membuat halaman todo_page dan membuat class TodoPage dengan StatelessWidget

---
+++ lib/presentation/todo_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class Todopage extends StatelessWidget {
+  const Todopage({super.key});
+  
+}
