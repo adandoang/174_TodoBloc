4d3bf6109ea18a08eaae7c6af6e3a351d33a88bf
adandoang <akhdanjzif@gmail.com>
2025-05-08 18:51:51 +0700

    inisialisasi key dan controller untuk form input

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -4,5 +4,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Todopage extends StatelessWidget {
   const Todopage({super.key});
-  
+
+  @override
+  Widget build(BuildContext context) {
+    final _key = GlobalKey<FormState>();
+    final _controller = TextEditingController();
+    
+  }
 }
